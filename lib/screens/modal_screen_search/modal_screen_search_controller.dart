0b8d9716c48bb54d1dbe8232d2e78e5acd2dc650
peterbang <peter.bang@social-chart.com>
2023-01-02 16:38:14 +0900

    full text search engine is being changed from typesense to elastic search cloud

--- lib/screens/modal_screen_search/modal_screen_search_controller.dart
+++ lib/screens/modal_screen_search/modal_screen_search_controller.dart
@@ -1,13 +1,22 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:elastic_app_search/elastic_app_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:socialchart/models/model_typesense_result.dart';
 import 'package:socialchart/models/typesense_search_instance.dart';
 
 class ModalScreenSearchController extends GetxController {
   TextEditingController searchFieldController = TextEditingController();
+
+  //elastic search
+  final service = ElasticAppSearch(
+      endPoint:
+          "https://social-chart-search.ent.asia-northeast3.gcp.elastic-cloud.com",
+      searchKey: dotenv.env['ELASTIC_APP_SEARCH_KEY']!);
+
   TypesenseSearchInstance searchInstance = TypesenseSearchInstance();
   final _chartList = Rx<List<TypesenseDocument>>([]);
   // final _dataSetList = Rxn<List<TypesenseDocument>>([]);
@@ -33,6 +42,21 @@ class ModalScreenSearchController extends GetxController {
       return;
     }
     _chartList.value = result.map((e) => e.document).toList();
+  }
+
+  void getElasticSearchResult(String value) async {
+    ElasticResponse response = await service
+        .engine("chartinfo")
+        .query(value.trim())
+        // .filter("states", isEqualTo: "California")
+        // .filter("world_heritage_site", isEqualTo: true)
+        // .resultField("title")
+        // .resultField("description", snippetSize: 140)
+        .page(1, size: 50)
+        .get();
+    for (ElasticResult result in response.results) {
+      print(result.data);
+    }
   }
 
   @override
